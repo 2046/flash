fdf96cb739aa38bd4604991dd21749689b8b6ad3
hanfeizi <han@hanfeizideMacBook-Air.local>
2014-09-02 16:24:22 +0800

    init

---
+++ src/class.js
@@ -0,0 +1,83 @@
+'use strict'
+
+function F(){};
+
+var createProto = (function(){
+    if(Object.__proto__){
+        return function(proto){
+            return {
+                __proto__ : proto
+            };
+        };
+    }else{
+        return function(proto){
+            F.prototype = proto;
+            return new F();
+        };
+    }
+})();
+
+function Class(properties){
+    return classify(implement.call(function(){
+        if(this.initialize){
+            this.initialize.apply(this, arguments);
+        }
+    }, properties || {}));
+};
+
+Class.Mutators = {
+    Extends : function(parent, proto){
+        proto = createProto(parent.prototype);
+        mix(proto, this.prototype);
+        proto.constructor = this;
+        this.prototype = proto;
+        this.superclass = parent.prototype;
+    },
+    Implements : function(items, item, proto){
+        proto = this.prototype;
+
+        while(item = items.shift()){
+            mix(proto, item.prototype || item);
+        }
+    }
+};
+
+function implement(properties){
+    var key, value;
+
+    for(key in properties){
+        value = properties[key];
+
+        if(Class.Mutators.hasOwnProperty(key)){
+            Class.Mutators[key].call(this, value);
+        }else{
+            this.prototype[key] = value;
+        }
+    }
+
+    return this;
+};
+
+function mix(receiver, supplier){
+    var key;
+
+    for(key in supplier){
+        if(supplier.hasOwnProperty(key) && key !== 'prototype'){
+            receiver[key] = supplier[key];
+        }
+    }
+};
+
+function extend(properties){
+    properties = properties || {};
+    properties.Extends = this;
+    return new Class(properties);
+};
+
+function classify(cls){
+    cls.extend = extend;
+    cls.implement = implement;
+    return cls;
+};
+
+module.exports = Class;
---
+++ test/class-test.js
@@ -0,0 +1,235 @@
+define(function(require, exports, module){
+    var Class, expect;
+
+    Class = require('class');
+    expect = require('expect');
+
+    function equals(){
+        var args = arguments;
+        expect(args[0]).to.equal(args[1]);
+    };
+
+    describe('Class', function(){
+        it('new Class', function(){
+            var Dog = new Class({
+                initialize : function(name){
+                    this.name = name;
+                },
+                getName : function(){
+                    return this.name;
+                }
+            });
+
+            var dog = new Dog('Jack');
+
+            equals(dog.constructor, Dog);
+            equals(dog.name, 'Jack');
+            equals(dog.getName(), 'Jack');
+        });
+
+        it('new Class(null)', function(){
+            var Dog = new Class(null);
+            var dog = new Dog();
+
+            Dog = new Class();
+            dog = new Dog();
+
+            equals(dog.constructor, Dog);
+        });
+
+        it('Extends', function(){
+            var Animal = new Class({
+                initialize : function(name){
+                    this.name = name;
+                },
+                getName : function(){
+                    return this.name;
+                }
+            });
+
+            var Dog = new Class({
+                Extends : Animal,
+                talk : function(){
+                    return 'I am ' + this.name;
+                }
+            });
+
+            var dog = new Dog('Jack');
+            equals(dog.name, 'Jack');
+            equals(dog.getName(), 'Jack');
+            equals(dog.talk(), 'I am Jack');
+        });
+
+        it('call initialize method property', function(){
+            var counter = 0;
+
+            var Animal = new Class({
+                initialize : function(){
+                    counter++;
+                }
+            });
+
+            var Dog = new Class({
+                Extends : Animal,
+                initialize : function(){
+                    counter++;
+                }
+            });
+
+            new Dog();
+
+            equals(counter, 1);
+
+            counter = 0;
+
+            Dog = new Class({
+                Extends : Animal
+            });
+
+            new Dog();
+
+            equals(counter, 1);
+        });
+
+        it('pass arguments to initialize method property', function(){
+            var Animal = new Class({
+                initialize : function(firstName, lastName){
+                    this.fullName = firstName + ' ' + lastName;
+                }
+            });
+
+            var Bird = Animal.extend({
+                fly : function(){}
+            });
+
+            var bird = new Bird('hello', 'world');
+            equals(bird.fullName, 'hello world');
+        });
+
+        it('superclass', function(){
+            var counter = 0;
+
+            var Animal = new Class({
+                initialize : function(){
+                    counter++;
+                },
+                talk : function(){
+                    return 'I am an animal';
+                }
+            });
+
+            var Dog = new Class({
+                Extends : Animal,
+                initialize : function(){
+                    Dog.superclass.initialize();
+                },
+                talk : function(){
+                    return Dog.superclass.talk();
+                }
+            });
+
+            var dog = new Dog();
+            equals(counter, 1);
+            equals(dog.talk(), 'I am an animal');
+        });
+
+        it('Implements', function(){
+            var Animal = new Class({
+                initialize : function(name){
+                    this.name = name;
+                },
+                getName : function(){
+                    return this.name;
+                }
+            });
+
+            var Flyable = {
+                fly : function(){
+                    return 'I am flying';
+                }
+            };
+
+            var Talkable = function(){};
+
+            Talkable.prototype.talk = function(){
+                return 'I am ' + this.name;
+            };
+
+            var Dog = new Class({
+                Extends : Animal,
+                Implements : [Flyable, Talkable]
+            });
+
+            var dog = new Dog('Jack');
+
+            equals(dog.name, 'Jack');
+            equals(dog.getName(), 'Jack');
+            equals(dog.fly(), 'I am flying');
+            equals(dog.talk(), 'I am Jack');
+        });
+
+        it('SubClass.extend', function(){
+            var Animal = new Class({
+                initialize : function(name){
+                    this.name = name;
+                }
+            });
+
+            var Dog = Animal.extend();
+            var dog = new Dog('Jack');
+
+            equals(dog.name, 'Jack');
+            equals(Dog.superclass.constructor, Animal);
+        });
+
+        it('SubClass.implement', function(){
+            var Animal = new Class({
+                initialize : function(name){
+                    this.name = name;
+                }
+            });
+
+            var Dog = Animal.extend();
+
+            Dog.implement({
+                talk : function(){
+                    return 'I am ' + this.name;
+                }
+            });
+
+            var dog = new Dog('Jack');
+
+            equals(dog.name, 'Jack');
+            equals(dog.talk(), 'I am Jack');
+            equals(Dog.superclass.constructor, Animal);
+        });
+
+        it('new AnotherClass() in initialize', function(){
+            var called = [];
+
+            var Animal = new Class({
+                initialize : function(){
+                    called.push('Animal');
+                }
+            });
+
+            var Pig = new Class({
+                Extends : Animal,
+                initialize : function(){
+                    called.push('Pig');
+                }
+            });
+
+            var Dog = new Class({
+                Extends : Animal,
+                initialize : function(){
+                    new Pig();
+                    called.push('Dog');
+                }
+            });
+
+            new Dog();
+            equals(called.join(' '), 'Pig Dog');
+        });
+    });
+});
